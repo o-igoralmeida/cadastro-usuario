c17ed7a98e93d794f49db21505d0fa92898420fe
o-igoralmeida <igor.almeidaa457@gmail.com>
2026-01-28 16:10:06 -0300

    Initial commit

---
+++ cadastro-usuario/src/main/java/com/igoralmeida/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.igoralmeida.cadastro_usuario.infrastructure.repository;
+
+import com.igoralmeida.cadastro_usuario.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
